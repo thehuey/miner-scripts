141b6343a261400c3781e37b68211dc52ebde855
Zhihui Hu <thehuey@gmail.com>
2021-10-25 05:53:23 +0000

    update fan control to check for card device path

--- card_fan.sh
+++ card_fan.sh
@@ -2,5 +2,17 @@
 lvl=$( echo "$2 * 2.5" | bc | awk '{printf("%d\n", $1)}' )
 mon=$( echo "$1 + 1" | bc )
 echo "Setting card $1 fan to: $2% Value: $lvl"
-sh -c "echo 1 > /sys/class/drm/card$1/device/hwmon/hwmon$mon/pwm1_enable"
-sh -c "echo "$lvl" > /sys/class/drm/card$1/device/hwmon/hwmon$mon/pwm1"
+
+#for i in $(lspci -D |grep VGA |grep AMD |cut -f 1 -d' '|cut -f 1 -d'.'); 
+#do 
+#card=$(ls -l1d /sys/class/drm/card? |grep "card$i" |cut -f 9 -d ' ') 
+#echo "Card $card"
+#done
+card=$(ls -l1d /sys/class/drm/card$1/device/hwmon/hwmon? |cut -f 9 -d ' ')
+if [ -z "$card" ]; then
+  echo "Directory is empty"
+else
+  echo "Directory: $card"
+  sh -c "echo 1 > $card/pwm1_enable"
+  sh -c "echo "$lvl" > $card/pwm1"
+fi
--- fan_65.sh
+++ fan_65.sh
@@ -1,11 +1,22 @@
 #/bin/sh
 lvl=$( echo "$1 * 2.5" | bc | awk '{printf("%d\n", $1)}' )
-echo "Setting fan to: $1% Value: $lvl"
-echo 1 > /sys/class/drm/card0/device/hwmon/hwmon1/pwm1_enable
-echo "$lvl" > /sys/class/drm/card0/device/hwmon/hwmon1/pwm1
-echo 1 > /sys/class/drm/card1/device/hwmon/hwmon2/pwm1_enable
-echo "$lvl" > /sys/class/drm/card1/device/hwmon/hwmon2/pwm1
-echo 1 > /sys/class/drm/card2/device/hwmon/hwmon3/pwm1_enable
-echo "$lvl" > /sys/class/drm/card2/device/hwmon/hwmon3/pwm1
-echo 1 > /sys/class/drm/card3/device/hwmon/hwmon4/pwm1_enable
-echo "$lvl" > /sys/class/drm/card3/device/hwmon/hwmon4/pwm1
+echo "Setting fans to: $1% Value: $lvl"
+for i in $(lspci -D |grep VGA |grep AMD |cut -f 1 -d' '|cut -f 1 -d'.'); 
+do 
+ card=$(ls -l1d /sys/class/drm/card? |grep $i |cut -f 9 -d ' ') 
+ echo "Card $card"
+ mondir=$(ls -l1d $card/device/hwmon/hwmon? | cut -f 9 -d ' ')
+ echo $mondir
+ echo 1 > $mondir/pwm1_enable
+ echo "$lvl" > $mondir/pwm1
+done
+
+
+#echo 1 > /sys/class/drm/card0/device/hwmon/hwmon1/pwm1_enable
+#echo "$lvl" > /sys/class/drm/card0/device/hwmon/hwmon1/pwm1
+#echo 1 > /sys/class/drm/card1/device/hwmon/hwmon2/pwm1_enable
+#echo "$lvl" > /sys/class/drm/card1/device/hwmon/hwmon2/pwm1
+#echo 1 > /sys/class/drm/card2/device/hwmon/hwmon3/pwm1_enable
+#echo "$lvl" > /sys/class/drm/card2/device/hwmon/hwmon3/pwm1
+#echo 1 > /sys/class/drm/card3/device/hwmon/hwmon4/pwm1_enable
+#echo "$lvl" > /sys/class/drm/card3/device/hwmon/hwmon4/pwm1
